67c1d1f57eb7bd87dd513222e95b2a0b8d4215e3
sohail <ssohailmails@gmail.com>
2018-07-19 22:15:07 +0530

    OpeningSwiftApp

---
+++ MyTrainingManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTrainingManager
+//
+//  Created by SOHAIL UR RAHMAN on 19/07/18.
+//  Copyright © 2018 SOHAIL UR RAHMAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
